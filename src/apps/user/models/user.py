1bd18108d543f709384983508e242abbcf0ebb57
Rauan55 <asetovrauan95@gmail.com>
2025-03-30 12:10:40 +0500

    User Model

---
+++ src/apps/user/models/user.py
@@ -0,0 +1,21 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.libs.base_model import Base
+
+
+class UserModel(Base):
+    """User Model
+
+    :param id: identifier
+    :param name: user name
+    :param surname: user surname
+    :param email: user email
+    :param password: user password hash
+    """
+    __tablename__ = "users"
+
+    name: Mapped[str] = mapped_column(String(20))
+    surname: Mapped[str] = mapped_column(String(20), nullable=True)
+    email: Mapped[str] = mapped_column(String(50), unique=True, index=True)
+    password: Mapped[str]
