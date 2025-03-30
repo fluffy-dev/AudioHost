0ab36b4a010a9cb23a448738afc810dfa321612c
Rauan55 <asetovrauan95@gmail.com>
2025-03-30 23:45:03 +0500

    production ready, somehow tested

--- migrations/env.py
+++ migrations/env.py
@@ -23,6 +23,9 @@ if config.config_file_name is not None:
 from migrations import models
 target_metadata = models.Base.metadata
 
+from src.config.database.settings import settings
+config.set_main_option("sqlalchemy.url", settings.database_url)
+
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
