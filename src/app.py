506e55009eb5a9ea4f281918eb83fc36eeeca990
Assetov Rauan <74526015+fluffy-dev@users.noreply.github.com>
2025-03-31 10:43:08 +0500

    forgot to enable logging

--- src/app.py
+++ src/app.py
@@ -1,8 +1,13 @@
+import logging
 from fastapi import FastAPI
 
 from src.api.routes import router
 
+from src.config.logging import settings as logger_settings, logger_config
+
 def get_app() -> FastAPI:
+    if logger_settings.logging_on:
+        logging.config.dictConfig(logger_config)
     app = FastAPI()
     app.include_router(router)
     return app
